bc82e483599d7654892f3a1404cc09e48bd0d12a
Mark Huot <mark@markhuot.com>
2011-10-07 16:00:59 -0400

    getting it into the db

--- models/recognize_model.php
+++ models/recognize_model.php
@@ -5,19 +5,21 @@ class Recognize_model extends CI_Model
 	
 	public function allow($client_id)
 	{
-		$code = 
-		$access_token = 
-		$token_type = 
-		$expires_in = time() + (60 * 10); /* 10 minutes */
-		$scope = $this->EE->input->get('scope');
+		$code = '';
+		$access_token = '';
+		$token_type = '';
+		$expires_in = 60 * 10; /* 10 minutes */
+		$expires_at = time() + $expires_in;
+		$scope = $this->input->get('scope');
 		
-		$this->EE->db->set('member_id', $this->EE->session->userdata('member_id'));
-		$this->EE->db->set('app_id', $client_id);
-		$this->EE->db->set('code', $code);
-		$this->EE->db->set('access_token', $access_token);
-		$this->EE->db->set('token_type', $token_type);
-		$this->EE->db->set('expires_in', $expires_in);
-		$this->EE->db->set('scope', $scope);
+		$this->db->set('member_id', $this->session->userdata('member_id'));
+		$this->db->set('app_id', $client_id);
+		$this->db->set('code', $code);
+		$this->db->set('access_token', $access_token);
+		$this->db->set('token_type', $token_type);
+		$this->db->set('expires_at', $expires_at);
+		$this->db->set('scope', $scope);
+		$this->db->insert('exp_recognize_auths');
 	}
 	
 }
